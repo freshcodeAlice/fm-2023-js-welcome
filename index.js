1adcbbe092ae72a306e3fc5404b61b5582890e9f
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-13 10:57:48 +0200

    const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var - застарілий спосіб, не використовуємо
 
 let - створює змінну
-const
+const - створює константу, значення якої не можна змінювати
 
 
 */
@@ -13,3 +13,9 @@ let variable;  // camelCase - superImportantVariable
 variable = 3;
 /////// there is more code
 variable = 'hello';
+
+
+const a = 1;
+//a = 3;
+
+const PI = 3.14;
