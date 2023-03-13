bccaf1493c4f31f6011c68afb893ae38c1065c10
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-13 10:51:47 +0200

    variables

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+var - застарілий спосіб, не використовуємо
+
+let - створює змінну
+const
+
+
+*/
+
+
+
+let variable;  // camelCase - superImportantVariable
+variable = 3;
+/////// there is more code
+variable = 'hello';
