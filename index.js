a6ad785e635090a176e0c94cd885579b216a5ae3
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-13 11:02:39 +0200

    naming rules

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ const a = 1;
 //a = 3;
 
 const PI = 3.14;
+
+/*
+Naming
+
+
+
+translit - BAD PRACTICE
+
+Use english words in naming
+
+1value - DONT
+value1 - ok
+
+$ - jquery
+_ - lodash
+
+*/
