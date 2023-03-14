531086bd4fa42a0c744c8b52198f701a448eca65
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-14 11:54:19 +0200

    Operator shotcuts

--- index.js
+++ index.js
@@ -1,65 +1,13 @@
-// Оператор if  - умовний оператор
-// виконує блок коду, якщо умова = true
-
-/*
-if (умова) {
-    блок коду, який виконається, якщо умова = true
-}
-
-*/
-
-/*
-let age = 20;
-
-if (age > 18) {
-    alert('You are adult');
-    age = 10;
-    console.log(age);
-}
-*/
-
-/* Створіть змінні cat = 1 , dog = 2, rat = 4
-Виведіть кількість (суму) домашніх тварин, якщо їх більше ніж 2
-*/
-
-///// 
-//age = 5;
-//console.log(age); 
+// Скорочення для арифметичних операцій з присвоюванням
 
 
+let box = 10;
 
-/*
-if (умова) {
-    блок коду, який виконається, якщо умова === true
-} else {
-    блок коду, який виконається, якщо умова === false
-}
+box = box + 5;
+box += 5;
 
-*/
+box = box * 2;
+box *= 2;
 
-
-let userAge = 15;
-
-if (userAge > 18) {
-    alert('You are adult');
-} else {
-    alert('You are not enought adult');
-}
-
-
-/*
-Створіть змінну appleColor = 'green'
-
-Якщо колір яблука - червоний, то виведіть на консоль фразу "Яблуко червоне", в іншому випадку виведіть "Яблуко іншого кольору"
-
-
-*/
-
-let appleColor = 'green';
-
-
-if (appleColor === 'red') {
-    console.log('Apple is red')
-} else {
-    console.log('Apple has another color');
-}
+box = box / 3;
+box /= 3;
