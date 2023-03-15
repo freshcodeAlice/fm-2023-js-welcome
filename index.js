3e954cb924440fc8e59dcb25856c7cb02bb6acf9
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-15 11:38:02 +0200

    tasks resolve

--- index.js
+++ index.js
@@ -12,15 +12,42 @@ function ім'яФункції(список аргументів) {
 */
 
 
-function washMachine(mode, temperature, rmp, soap, clothes) {
-        // робимо прання 
 
-        let pureClothes = 'Its pure';
-        ///////
-        return pureClothes
+/*
+Напишіть функцію, яка приймає два числа (послідовно два prompt) і виводить в консоль суму цих двох чисел
+
+
+*/
+
+
+function getSum(number1, number2) {
+    // console.log(number1+number2);
+    return number1 + number2;
 }
 
 
+let num1 = Number(prompt('Type first number'));
+let num2 = Number(prompt('Type second number'));
 
-let resultOfWashing = washMachine('Bavovna', 30, 400, 'Soap', 'Shirt');
-console.log(resultOfWashing);
+
+let res = getSum(num1, num2);
+console.log(res);
+
+
+/*
+Задачка з зірочкою - прийняти від користувача два числа і повернути те, що більше
+*/
+
+
+let n1 = Number(prompt('First number'));
+let n2 = Number(prompt('Second number'));
+
+function getBigger(num1, num2) {
+    // if (num1 > num2) {
+    //     return num1
+    // } else {
+    //     return num2
+    // }
+
+    return (num1 > num2) ? num1 : num2
+}
