e13c2e3621ff0eee8ee61715e5cd81b0ac3f9b9d
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-15 10:59:58 +0200

    function welcome

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
-let num = Number(prompt('Type your number')); //'12' -> 12
-// num = 'aaaaaa',  Number('aaaaaa')  -> NaN
-let verdict = isNaN(num); // isNaN(12) --> false
+/* Функції  */
 
-if (verdict) {
-    console.log('Its not a number')
-} else {
-    console.log(num + 2);
+// Функція - це окремий, ізольований фрагмент коду, який має ім'я і за цим ім'ям до нього можна звернутись з різних частин загального коду
+
+// 1 спосіб створення функції - Function Declaration
+
+/*
+function ім'яФункції(список аргументів) {
+    блок коду функції
 }
 
+*/
+
+function sumTwoAndTwo(){
+    let sum = 2 + 2;
+    console.log(sum);
+}
+/////////////////
+
+
+// sumTwoAndTwo();
+
+
+ function sumThreeAndThree() {
+    let sum2 = 3 + 3;
+    console.log(sum2);
+ }
+
+
+// sumThreeAndThree();
+
+
+function sumTwoNumbers(arg1, arg2){
+        let sum = arg1 + arg2;
+        console.log(sum);
+}
+
+
+sumTwoNumbers(2, 3);
+
+sumTwoNumbers(5, 10);
+
+//console.log(sum);
+
+
+
+function something(a1, a2, a3, a4, a5) {
+    console.log(a1);
+    console.log(a2);
+    console.log(a3);
+    console.log(a4);
+    console.log(a5);
+
+    console.log(a1 + a2 + a3 + a4 + a5);
+}
+
+
+something(1, 2, 3);
+
+
+
+function something2() {
+    console.log('hello');
+}
+
+something2(1, 2, 3);
