c823e57c371f129a891c9c8bcdb8ba5ebc54b095
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-16 11:45:33 +0200

    if - else if - else

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+/*  if - else  */
+/* if - else if - else */
 
-function sayHello(name = 'Anonym') { // аргументи за замовчуванням
-    console.log('Hello, ' + name);
+function getDivider(number) {
+    //  ПОГАНИЙ КОД!
+    // if (number % 2 === 0) {
+    //     console.log('Число ділиться на 2')
+    // } else {
+    //     if (number % 3 === 0) {
+    //         console.log('Число ділиться на 3')
+    //     } else {
+    //         if (number % 5 === 0) {
+    //             console.log('Число ділиться на 5')
+    //         }
+    //     }
+    // }
+  
+    /*  if - else  */
+    /* if - else if - else */
+
+    if (number % 2 === 0) {
+     console.log('Число ділиться на 2')
+    } else if (number % 3 === 0) {
+     console.log('Число ділиться на 3')
+    } else if (number % 5 === 0) {
+     console.log('Число ділиться на 5')
+    } else {
+     console.log('Число не ділиться ані на 2, ані на 3, ані на 5')
+    }
+   
 }
-
-sayHello();
