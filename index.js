e0c665cf5edf19ed4c1d9cdb4052b3b0c15db256
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-16 09:59:53 +0200

    3 types of functions

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+// 1 - Function Declaration - Задекларована функція
 
 /*
-Написати функцію, яка приймає радіус окружності і повертає довжину окружності
+funName() // таку функцію можна викликати раніше, ніж в коді вона описана
+
+
+function funName(arg) {
+    //// code
+    return ;
+}
+
 
 */
 
 
-function circumference(r) {
-    const PI = 3.14;
-    let circum = 2 * PI * r;
-    return circum;
+// 2 - Function Expression - Функціональний вираз
+
+let func = function(arg) {
+    ////
 }
+
+func() // таку функцію можна викликати тільки після того, як вона описана (тобто нижче по коду)
+
+
+// 3 - Arrow function - Стрілочні функції
+
+let arrow = (arg) => {
+    /////
+}
+
+let shotArrow = x => x * x; // Якщо стрілочна функція складається з одного рядка, то можна опустити слово return і {}
