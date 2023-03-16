aec9780621060677456f57bee557d6fde9fb92cf
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-16 09:51:49 +0200

    circumference task resolve

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
+
 /*
-Написати функцію, яка вирішує квадратні рівняння
-
-Квадратне рівняння - рівняння вигляду
-
-a*x**2 + b*x + c = 0
-
-Вирішується дискримінантом
-D = b2 − 4ac
-
-Якщо D > 0, то коренів 2,
-    x1 = (-b + ^D) / 2*a
-    x2 = (-b - ^D) / 2*a
-Якщо D = 0, то корінь 1
-Якщо D < 0, то коренів немає
+Написати функцію, яка приймає радіус окружності і повертає довжину окружності
 
 */
 
 
-function quadraticEquation(a, b, c) {
-    let D = b*b - 4 * a * c; 
-    let x1 = null;
-    let x2 = null;
-    if (D > 0) {
-        x1 = (-b + Math.sqrt(D)) / (2 * a);
-        x2 = (-b - Math.sqrt(D)) / (2 * a);
-        console.log(x1);
-        console.log(x2);
-    }
-    if (D === 0) {
-        x1 = -b / 2*a;
-        console.log(x1);
-    }
-    if (D < 0) {
-        console.log('Equation has no roots')
-    }
+function circumference(r) {
+    const PI = 3.14;
+    let circum = 2 * PI * r;
+    return circum;
 }
