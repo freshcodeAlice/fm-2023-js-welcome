739d169e1fff4e7efdd3903a77141eed9feb21b3
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-16 11:33:03 +0200

    Default arguments

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-/*
-Scope - область видимості. Це ті значення, до яких ми маємо доступ
 
-
-*/
-
-
-debugger;
-
-let counter = 1;
-
-
-function goodFunction(counter) {
-    console.log(counter); // Функція має свій counter у локальній області видимості і не буде лізти до глобальної
-    /// Все, з чим працює функція, має приходити цій функції як аргументи
-    
+function sayHello(name = 'Anonym') { // аргументи за замовчуванням
+    console.log('Hello, ' + name);
 }
 
-goodFunction(10);
+sayHello();
