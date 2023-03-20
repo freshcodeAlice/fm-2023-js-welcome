970e1c58f6b7a1b3b659faedb52e37de24d96a53
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-20 09:32:26 +0200

    Tasks resolve

--- index.js
+++ index.js
@@ -16,3 +16,78 @@ https://ru.wikipedia.org/wiki/Fizz_buzz
 Повторювати дію, поки користувач не введе правильне число.
 
 */
+
+
+/// FizzBuzz
+
+function fizzBuzz(num) {
+    for(let i = 1; i <= num; i++){
+        if ( i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+function fizzBuzz2(num) {
+    for(let i = 1; i <= num; i++){
+        if ( i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+            continue
+        } 
+        if (i % 3 === 0) {
+            console.log('Fizz');
+        } 
+        if (i % 5 === 0) {
+            console.log('Buzz');
+        } 
+        console.log(i);
+    }
+}
+
+
+///2
+
+function guessNum() {
+    let num = 5;
+    while(true) {
+            let userGuess = Number(prompt('Guess number'));
+            if ( userGuess === num) {
+                alert('You successfully guess!');
+                break;
+            } else if (userGuess > num) {
+                alert('Too much');
+            } else if (userGuess < num) {
+                alert('Not enough')
+            } else {
+                console.log('Error');
+            }
+    }
+}
+
+
+
+/*
+Нова задача:
+
+Написати функцію, яка зводить число у заданий ступінь. Зробити це не оператором зводу у ступінь (**), а саме циклом
+
+2^2 = 2*2
+2^3 = 2*2*2
+*/
+
+
+
+
+function pow(base, powNumber) {
+    let result = 1;
+    for(let i = 1; i <= powNumber; i++) {
+        result *= base;
+    }
+    return result;
+}
