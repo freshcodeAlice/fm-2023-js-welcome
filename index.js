9440addbfcc0bdf7546b52b3504fa2acf9f662b0
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-20 11:08:36 +0200

    Object manipulation

--- index.js
+++ index.js
@@ -1,72 +1,83 @@
-//// Типи даних
-
-let num = 5; // number
-let str = 'hello'; // string
-let bool = true; // boolean
-let n = null; // null
-let und = undefined; // undefined
-let bigint = 1n; // BigInt
-let symb = Symbol(); // Symbol
+// Копіювання і робота з об'єктом
 
 
-/// Object
 
-let obj = {
-    key1: 'hello',
-    key2: 5,
-    test: true,
-    insObj: {
-        key5: 'value'
+/// Робота з примітивними типами відбувається за значенням
+
+let a = 1;
+//undefined
+let b = a;
+//undefined
+a;
+//1
+b;
+//1
+a === b;
+//true
+1 === 1;
+//true
+
+a = 5;
+a // 5
+b // 1
+
+/// Робота з об'єктами відбувається за посиланням у пам'яті
+
+let obj1 = {
+    test: 'value'
+}
+
+let obj2 = obj1;
+    
+obj1.test = 'hello'
+//    'hello'
+ obj1
+//    {test: 'hello'}test: "hello"[[Prototype]]: Object
+obj2.test = 'new value'
+//    'new value'
+obj1
+//    {test: 'new value'}test: "new value"[[Prototype]]: Object
+obj2 === obj1
+//    true
+
+/// Не дорівнюють, бо це два різні пусті об'єкти
+let o1 = {};
+//undefined
+let o2 = {};
+//undefined
+o1 === o2
+//false
+
+
+
+
+/////
+
+
+function sayHello(obj) {
+    obj.age = 15;
+}
+
+let user = {
+    name: 'John',
+    hello: function() {
+        console.log('John say hello');
     }
 }
 
+sayHello(user)
 
-let fn = function() {
 
+
+
+function sumTwoNumbers(a, b) {
+   return a + b;
 }
 
-fn();
-
-/*
-Об'єкт = набір пар "ключ-значення"
-Ключ (властивість, поле) - це може бути або рядок, або тип даних Symbol
-
-Властивість - дані про об'єкт
-Метод - це функція, яка вбудована в об'єкт і передбачає певні дії з ним
-*/
-
-let cat = {
-    color: 'red',
-    breed: 'Dvorovyi',
-    insolenceLevel: 5,
-    name: 'Murzik',
-    weight: 5,
-    sex: 'male',
-    sayMeow: function(){
-        console.log('MEOOOWWWWWW');
-        return true;
-    },
-    sleep: function() {
-        console.log('Im sleeping, zzzz');
-    }
+function consoleResult(arg1, arg2, func) {
+    let result = func(arg1, arg2);
+    console.log(result);
 }
 
-///  Щоб отримати значення з властивості, використовуємо оператор доступу до властивості за назвою - "."
 
-/// ім'яОб'єкта.ім'яВластивості
-
-
-
-
-/// Зробіть кота, опишіть його властивості, додайте метод sayMeow та sleep
-
-
-
-/* CRUD над об'єктом
-
-
-Create (створити нову властивість): cat.age = 1;
-Read (прочитати): cat.age
-Update (оновити): присвоїти нове значення: cat.name = 'Barsik'
-Delete (видалити): delete cat.sleep
-*/
+consoleResult(4, 5, sumTwoNumbers)
