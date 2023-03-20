d0482aa68c3a64325ebb94046f9a37ee722f37c0
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-20 11:37:17 +0200

    TemplateString

--- index.js
+++ index.js
@@ -1,90 +1,3 @@
-// Копіювання і робота з об'єктом
-
-
-
-/// Робота з примітивними типами відбувається за значенням
-
-let a = 1;
-//undefined
-let b = a;
-//undefined
-a;
-//1
-b;
-//1
-a === b;
-//true
-1 === 1;
-//true
-
-a = 5;
-a // 5
-b // 1
-
-/// Робота з об'єктами відбувається за посиланням у пам'яті
-
-let obj1 = {
-    test: 'value'
-}
-
-let obj2 = obj1;
-    
-obj1.test = 'hello'
-//    'hello'
- obj1
-//    {test: 'hello'}test: "hello"[[Prototype]]: Object
-obj2.test = 'new value'
-//    'new value'
-obj1
-//    {test: 'new value'}test: "new value"[[Prototype]]: Object
-obj2 === obj1
-//    true
-
-/// Не дорівнюють, бо це два різні пусті об'єкти
-let o1 = {};
-//undefined
-let o2 = {};
-//undefined
-o1 === o2
-//false
-
-
-
-
-/////
-
-
-function sayHello(obj) {
-    obj.age = 15;
-}
-
-let user = {
-    name: 'John',
-    hello: function() {
-        console.log('John say hello');
-    }
-}
-
-sayHello(user)
-
-
-
-
-function sumTwoNumbers(a, b) {
-   return a + b;
-}
-
-function consoleResult(arg1, arg2, func) {
-    ///// виконати певну роботу, і тільки після неї викликати функцію, що була нам передана
-    let result = func(arg1, arg2);
-    console.log(result);
-}
-
-
-consoleResult(4, 5, sumTwoNumbers)
-
-
-
 /// Створіть два об'єкти користувачів, що мають ім'я та прізвище.
 // Напишіть функцію, яка приймає об'єкт і виводить на консоль вітання типу "ім'яЮзера каже Привіт"
 
@@ -102,4 +15,28 @@ let user2 = {
 
 function sayHello(obj) {
     console.log(obj.name + ' каже Привіт');
+}
+
+
+/// Шаблонні рядки
+
+let name = 'Vasia';
+
+let str = 'hello ' + name;
+
+console.log(str);
+
+let templateString = `hello ${name}`;
+
+console.log(templateString);
+
+let templ2 = `2 + 2 = ${2+2}`;
+
+
+console.log(templ2)
+
+
+
+function sayHello2(obj) {
+    console.log(`${obj.name} каже: "Привіт"`);
 }
