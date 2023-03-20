a977375d6b71be6fb9a7e6b0a16f2bffca995e22
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-20 10:29:57 +0200

    Object welcome

--- index.js
+++ index.js
@@ -1,10 +1,72 @@
+//// Типи даних
+
+let num = 5; // number
+let str = 'hello'; // string
+let bool = true; // boolean
+let n = null; // null
+let und = undefined; // undefined
+let bigint = 1n; // BigInt
+let symb = Symbol(); // Symbol
+
+
+/// Object
+
+let obj = {
+    key1: 'hello',
+    key2: 5,
+    test: true,
+    insObj: {
+        key5: 'value'
+    }
+}
+
+
+let fn = function() {
+
+}
+
+fn();
+
 /*
-Написати функцію, яка обчислює факторіал числа
+Об'єкт = набір пар "ключ-значення"
+Ключ (властивість, поле) - це може бути або рядок, або тип даних Symbol
 
-n! = 1*2*3*4*5.....*n
+Властивість - дані про об'єкт
+Метод - це функція, яка вбудована в об'єкт і передбачає певні дії з ним
+*/
 
-5! = 1*2*3*4*5
+let cat = {
+    color: 'red',
+    breed: 'Dvorovyi',
+    insolenceLevel: 5,
+    name: 'Murzik',
+    weight: 5,
+    sex: 'male',
+    sayMeow: function(){
+        console.log('MEOOOWWWWWW');
+        return true;
+    },
+    sleep: function() {
+        console.log('Im sleeping, zzzz');
+    }
+}
 
-8! = 1*2*3*4*5*6*7*8
+///  Щоб отримати значення з властивості, використовуємо оператор доступу до властивості за назвою - "."
 
+/// ім'яОб'єкта.ім'яВластивості
+
+
+
+
+/// Зробіть кота, опишіть його властивості, додайте метод sayMeow та sleep
+
+
+
+/* CRUD над об'єктом
+
+
+Create (створити нову властивість): cat.age = 1;
+Read (прочитати): cat.age
+Update (оновити): присвоїти нове значення: cat.name = 'Barsik'
+Delete (видалити): delete cat.sleep
 */
