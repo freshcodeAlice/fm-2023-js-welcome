c7bd7bdaa226420384adb6e2799d41592e8626b4
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-21 09:59:26 +0200

    object entity equals

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+let car = {
+    brand: 'Audi',
+    type: 'sedan',
+    volume: 1.6,
+    capasity: 5
+}
 
-let age = 15;
-let name = 'John';
 
-let user = {
-    age,
-    name
+const isColor = 'color' in car;
+
+for (const key in car) {
+    console.log(key, car[key]);
+}
+
+
+///// Задача: порівняти вміст двох об'єктів
+
+const o1 = {
+    test: 'value',
+    color: 'red',
+    weight: 5
+}
+
+
+const o2 = {
+    test: 'value',
+    color: 'red',
+    weight: 5
+}
+
+
+function isObjEquals(obj1, obj2) {
+    for (const key in obj1) {
+       if(obj1[key] !== obj2[key]) {
+        return false
+       }
+    }
+    return true;
 }
