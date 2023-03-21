48220d43d45fae630bf9dfef62a78876475dfaf6
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-21 11:08:38 +0200

    Function-contrustor

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const obj = { // літеральний спосіб
-    key: 'value'
+const cat1 = {
+    name: 'Murk',
+    age: 5,
+    color: 'red'
+}
+
+const cat2 = {
+    name: 'Barsik',
+    age: 3,
+    color: 'grey'
+}
+
+const cat3 =  {
+    name: 'Kit',
+    age: 2,
+    color: 'black'
+}
+
+// Функція-конструктор - особлива функція, її ім'я пишеться з великої літери і вона створює і повертає новий об'єкт, що створюється за певним шаблоном
+
+
+function Cat(name, age, weight ,color){
+    // new -> створюється {}
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.sayMeow = function () {
+        return 'MEOWWWW'
+    }
 }
 
 
-const o2 = new Object();
+const blackCat = new Cat('Kitty', 3, 'black');
