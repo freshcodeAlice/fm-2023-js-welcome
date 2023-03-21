4d58cbcf18ea2d5caadf0905d8252e48975e69af
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-21 11:17:11 +0200

    this

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const cat1 = {
-    name: 'Murk',
-    age: 5,
-    color: 'red'
-}
-
-const cat2 = {
-    name: 'Barsik',
-    age: 3,
-    color: 'grey'
-}
-
-const cat3 =  {
-    name: 'Kit',
-    age: 2,
-    color: 'black'
-}
-
-// Функція-конструктор - особлива функція, її ім'я пишеться з великої літери і вона створює і повертає новий об'єкт, що створюється за певним шаблоном
-
-
-function Cat(name, age, weight ,color){
-    // new -> створюється {}
-    this.name = name;
-    this.age = age;
-    this.color = color;
-    this.sayMeow = function () {
-        return 'MEOWWWW'
+const cat = {
+    name: 'Murzik',
+    color: 'red',
+    askForFood: function() {
+        return `${this.name} wants to eat`
     }
-}
-
-
-const blackCat = new Cat('Kitty', 3, 'black');
-
-blackCat.age++;
-
-for(let i=0; i< 3; i++) {
-    const cat = new Cat('aaa','ccc', 'ssss');
-    console.log(cat)
 }
