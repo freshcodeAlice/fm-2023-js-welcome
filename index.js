98b6d45a73e3911ada8582e7cd5f4db51b3f98e3
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-21 09:40:48 +0200

    variables in obj

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const userPlan = {
-    mon: 'dantist',
-    tue: 'go to park',
-    wed: 'hard work',
-    thu: 'go party',
-    fri: 'read a book',
-    sat: 'stay in bed'
+
+let age = 15;
+let name = 'John';
+
+let user = {
+    age,
+    name
 }
-
-
-//// Спитати у юзера, який день тижня його цікавить
-/// Вивести alert-ом значення з об'єкта, що відповідає введеному дню.
-
-// Наприклад: юзер вводить 'Wed' -> 'hard work'
-
-const userInput = prompt('What day do you want?');
-alert(`${userPlan[userInput] ? userPlan[userInput] : 'Nothing planned'}`);
-
