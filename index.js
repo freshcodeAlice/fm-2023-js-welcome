560e1802bd450f97a7bfec755321692b2e11e024
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-22 10:22:29 +0200

    array welcome

--- index.js
+++ index.js
@@ -1,88 +1,83 @@
-function User(firstName, lastName, age, email) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.email = email;
-    this.sayHello = function() {
-        return `${this.firstName} say: 'Hello'`
-    }
+/* Масиви  */
+
+const arr = [1, 4, 8, 9];
+
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
 }
 
 
-const u1 = new User('John', 'Doe', 23, 'doe@ls.as');
-const u2 = new User('Jane', 'Crow', 34, 'dsa@ldsa');
-
 /*
-Написати функцію-конструктор, яка створює об'єкт Car
-У машини будуть наступні властивості:
-- brand
-- maxSpeed
-- speed
+Знайти суму елементів числового масива
 
-Окрім властивостей, у машини мають бути методи:
-
-.accelerate(value) // пришвидшує машину, якщо вона ще не досягла максимальної швидкості
-(це speed + value)
-.stop() // зупиняє машину (speed = 0)
-
-Обидва методи в результаті роботи мають повертати значення поточної швидкості
-
-Задачка з *:
-.deaccelerate(value) - зменшує швидкість на указану кількість
 */
 
 
-function Car(brand, maxSpeed = 200) {
-    this.brand = brand;
-    this.maxSpeed = maxSpeed;
-    this.speed = 0;
+let sum = 0;
 
-    this.accelerate = function(value) {
-        this.speed += value;
-        if (this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log(sum);
+
+
+
+/* Знайти найбільший числовий елемент у масиві */
+// [2, 3, 1, 6, 4] - 6
+
+
+function getMaxElement(array) {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (max < array[i]) {
+            max = array[i];
         }
-        return this.speed;
     }
+    return max;
+}
 
-    this.deaccelerate = function(value) {
-        this.speed -= value;
-        if(this.speed < 0) {
-            this.speed = 0;
+function getMinElement(array) {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (min > array[i]) {
+            min = array[i];
         }
-        return this.speed;
     }
+    return min;
+}
+console.log(getMinElement(arr));
 
-    this.stop = function() {
-        this.speed = 0;
-        return this.speed;
-    }
+
+/// Написати функцію, що знаходить середнє арифметичне всіх елементів масиву
+
+
+function getAverage(arr) {
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+return sum /  arr.length;
 }
 
 
-const bmv = new Car('BMV', 250);
+// Задачка з *: знайти суму всіх парних елементів масива
+// (за парними індексами)
+// [1, 2, 3, 4, 5, 6, 7]
+
+function getSumOfEven(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i = i + 2) {
+        sum += array[i];
+    }
+    return sum;
+}
 
 
-/*
-Домашнє завдання:
-1. Написати функцію-конструктор Worker(робітник). У робітника є
-- ім'я (firstName)
-- прізвище (lastName)
-- ставка за день (rate)
-- кількість відпрацьованих днів в поточному місяці (days)
+const arr2 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-А також метод getSalary, який розраховує зарплатню.
-
-Звернути увагу: ставка не може бути від'ємною, кількість відпрацьованих днів теж.
-
-
-
-2. Написати функцію-конструктор для країни Country
-Країна матиме властивості:
-- назва
-- площа
-- кількість населення
-
-Метод, що розраховує щільність населення (кількість населення / площу країни)
-
-*/
+console.log(getSumOfEven(arr2));
