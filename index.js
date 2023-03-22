55e83c6dcc0d5a488693993a1920895b2e5592bf
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-22 12:00:29 +0200

    filter + sort

--- index.js
+++ index.js
@@ -25,3 +25,39 @@ const array2 = [1, 2, 3, 4, 5];
 
 
 const changeArr = array2.map((num) => num+2);
+
+
+
+//////
+
+// array2.filter(function (element) {
+//     // if (element % 2 === 0) {
+//     //     return true
+//     // } else {
+//     //     return false
+//     // }
+
+//     // return (element % 2) ? false : true; 
+
+//     return element % 2 === 0
+// })
+
+const filtered = array2.filter((elem) => !(elem % 2));
+
+
+
+///// Sort
+
+
+const newArray = [2, 6, 3, 7, 1];
+
+newArray.sort((a, b) => b - a);
+
+
+users.sort(function (prev, next) {
+    if (prev.firstName > next.firstName) {
+        return 1;
+    } else {
+        return -1;
+    }
+});
