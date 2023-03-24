3ade7fb9f13e1c0afc93d128355b3ff9d590bf7a
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-24 11:51:47 +0200

    chaining example

--- index.js
+++ index.js
@@ -32,3 +32,70 @@ function MyProtoArray() {
 
 const protoArray = new MyProtoArray();
 MyArray.prototype = protoArray;
+
+
+
+/////// Прототипи - практика
+
+/*
+Написати функцію-конструктор для юзера.
+Юзер має:
+- ім'я
+- прізвище
+
+- метод, що повертає повне ім'я (ім'я + прізвище): `${this.name} ${this.lastName}`
+Метод має бути у прототипі об'єкта.
+
+*/
+
+function User(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+
+function UserPrototype() {
+    this.getFullName = function() {
+        return `${this.name} ${this.lastName}`
+    }
+}
+
+User.prototype = new UserPrototype();
+
+
+/*
+Написати функцію-конструктор для сходів ( Ladder)
+Є властивість:
+- step
+
+Методи:
+ - up() - піднятись на сходинку вище
+ - down() - спуститись на сходинку нижче
+ - showStep() - подивитись, на якій сходинці ми зараз є
+
+
+*/
+
+
+function Ladder(){
+    this.step = 0;
+}
+
+
+function LadderPrototype(){
+    this.up = function(){
+        this.step++;
+        return this;
+    }
+    this.down = function () {
+        this.step--;
+        return this;
+    }
+    this.showStep = function() {
+        return this.step;
+    }
+}
+
+Ladder.prototype = new LadderPrototype();
+
+///lddr.up().up().up().down(); - chaining (chain - ланцюжок, цепочка) - об'єднання викликів методів таким чином, що попередній виклик повертає об'єкт або значення, з яким відпрацьовує наступний метод
