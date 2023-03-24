7749b3a80d7dbd1778c0172893983015e4207a93
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-24 09:49:13 +0200

    Custom array methods

--- index.js
+++ index.js
@@ -1,55 +1,29 @@
+function MyArray() {
+    this.length = 0;
+
+    this.push = function(value) {
+        this[this.length] = value;
+        this.length++;
+    }
+
+    this.pop = function() {
+        // delete з об'єкта останній елемент
+        // зменшити довжину масива
+        const lastItem = this[this.length-1];
+       delete this[this.length-1];
+       this.length--;
+       return lastItem;
+    }
+}
+
 
 /*
-Домашнє завдання:
-1. Написати функцію-конструктор Worker(робітник). У робітника є
-- ім'я (firstName)
-- прізвище (lastName)
-- ставка за день (rate)
-- кількість відпрацьованих днів в поточному місяці (days)
-А також метод getSalary, який розраховує зарплатню.
-Звернути увагу: ставка не може бути від'ємною, кількість відпрацьованих днів теж.
+Реалізувати метод pop():
+this.value--;
 
 
-2. Написати функцію-конструктор для країни Country
-Країна матиме властивості:
-- назва
-- площа
-- кількість населення
-Метод, що розраховує щільність населення (кількість населення / площу країни)
+Задачка з *: 
+1. реалізувати метод forEach
+2. Реалізувати метод filter
+
 */
-
-
-function Worker(firstName, lastName, rate, days) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    if (rate < 0) {
-        this.rate = 0;
-    } else {
-        this.rate = rate;
-    }
-    if(days < 0) {
-        this.days = 0;
-    } else {
-        this.days = days;
-    }
-
-    this.getSalary = function(){
-        return this.days * this.rate;
-    }
-}
-
-
-
-///2
-
-
-function Country(name, area, popularity) {
-    this.name = name;
-    this.area = area;
-    this.popularity = popularity;
-
-
-    this.getDensity = function (){
-        return this.popularity/this.area;
-    } 
-}
