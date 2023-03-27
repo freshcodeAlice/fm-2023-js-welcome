26842394694a7bc921bd3fc43ee65517b1aa7b6e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-27 10:20:01 +0300

    try...catch

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 /// Виключення - Exceptions (Error)
 
 function pow(num, exp) {
-
     if(typeof num !== 'number' || typeof exp !== 'number'){
         throw new TypeError('Argument must be a number');
     }
-
     if(exp < 0) {
         throw new RangeError('Exp must be a positive number');
     }
-
     if (exp === 1) {
         return num
     }
@@ -21,8 +18,31 @@ function pow(num, exp) {
     return result;
 }
 
+/// try...catch
 
 /*
-Дописати цю функцію таким чином, щоби при передачі від'ємного ступеня викидалася відповідна помилка
+try {
+    /// блок коду, який потенційно може виплюнути помилку
+} catch(error) {
+    // блок коду, який виконається в разі, якщо в try ми зловили помилку. Об'єкт помилки буде лежати в error
+}
 
 */
+
+
+//pow(2, -1);
+
+
+try {
+    pow(2, -1);
+// якщо сталася помилка - блок  try не виконується далі, а одразу провалюється в catch
+    console.log('Try-try-try');
+} catch(myError) {
+    console.log('Some error happening');
+    console.log(myError.message);
+}
+
+
+pow()
+
+console.log('There is my additional code');
