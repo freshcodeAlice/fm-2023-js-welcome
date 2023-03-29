1e365aa2d6b8eb1fb055bfb83e5857096938418e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-29 11:37:17 +0300

    destructuration

--- index.js
+++ index.js
@@ -1,26 +1,93 @@
 'use strict'
 
-
-
-
-function sumAll(...restArr) {
-
-    let sum = 0;
-    for (let i = 0; i < restArr.length; i++) {
-        sum += restArr[i]
+const monitor = {
+   sizes: {
+    length: {
+        value: 20,
+        scale: 'sm'
+    },
+    width: {
+        value: 30,
+        scale: 'sm'
     }
+   },
+    brightness: '2000lm',
+   color: 'black',
+   manufacturer: {
+        name: 'Sony',
+        address: {
+            city: 'London',
+            street: 'Baker',
+            house: 27
+        }
+   }
 
-   return sum
 }
 
-/*
- Склейте масив [1, 3, 5, 7] та масив [2, 4, 6, 8], не використовуючи методу concat()
+// d = Math.sqrt(a*a + b*b);
 
- [1,3,5,7,2,4,6,8];
+// const lengthValue = monitor.sizes.length.value;
+const {sizes: {
+        length: {value: lengthValue},
+        width: {value: widthValue}
+        },
+        manufacturer: {
+            address: {
+                city: manufacturerCity
+            }
+        }
+    } = monitor;
 
-*/
+
+//const widthValue = monitor.sizes.width.value;
+//const manufacturerCity = monitor.manufacturer.address.city;
+
+const diagonal = Math.sqrt(lengthValue * lengthValue + widthValue * widthValue);
+
+console.log(diagonal);
 
 
-const arr1 = [1, 3, 5, 7];
-const arr2 = [2, 4, 6, 8];
-const concatedArray = [...arr1, ...arr2];
+const cat2 = {
+    name: 'Barsik',
+    girlfriend: {
+        name: 'Murka',
+        child: {
+            name: 'Kitty1'
+        }
+    }
+}
+
+
+/// Витягти в нові змінні імена подружки Барсіка і ім'я першого кошенятка
+
+const {girlfriend: {name: barsikGirl, child: {name: childName}}} = cat2;
+console.log(barsikGirl);
+console.log(childName);
+
+const array = [2, 3, 4, 5, 6];
+
+
+const [firstElem, secondElem, , , fourthElement] = array;
+console.log(firstElem);
+console.log(secondElem);
+console.log(fourthElement);
+
+
+
+const cat3 = {
+    name: 'Barsik',
+    girlfriend: {
+        name: 'Murka',
+        child: [{
+            name: 'Kitty1'
+        }, {
+            name: 'Kitty2'
+        }, {
+            name: 'Kitty3'
+        }]
+    }
+}
+
+const {girlfriend: {child: [{name: firstChildName}]}} = cat3;
+
+console.log(firstChildName);
