d3313625601622078e0383c71f295b19843401f4
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-29 09:10:35 +0300

    3 types of function (this)

--- index.js
+++ index.js
@@ -1,96 +1,23 @@
-class Figure {
-    constructor(type) {
-        this.type = type;
-    }
+'use strict'
 
-    getArea() {
-
-    }
+// function declaration
+function test() {
+    console.log(this);
 }
 
-class Triangle extends Figure{
-    constructor(a, h) {
-        super('triangle');
-        this.a = a;
-        this.h = h;
-    }
+test()
 
-    set a (value) {
-        this._a = value;
-    }
-
-    get a() {
-        return this._a;
-    }
-
-    set h(value) {
-        this._h = value;
-    }
-
-    get h() {
-        return this._h;
-    }
-
-// Перевизначення (перезапис) батьківського методу - override
-    getArea() {
-        return (this.a * this.h) / 2;
-    }
+// Function expression
+const fn2 = function () {
+    console.log(this);
 }
 
-/*
-Написати клас Квадрат (Square)
-додати метод обчислення площі квадрату
-
-*/
+fn2();
 
 
-class Square extends Figure {
-    constructor(a) {
-        super('square');
-        this.a = a;
-    }
-
-    getArea() {
-        return this.a * this.a;
-    }
+/// Arrow function
+const arrfn = () => {
+    console.log(this);
 }
 
-
-/////////
-
-/**
- * 
- * @param {Figure} figure 
- * @returns 
- */
-
-function getFigureArea(figure) {
-    if (figure instanceof Figure) {
-        return figure.getArea()
-    }
-
-}
-
-
-/*
-Поліморфізм - здатність функції працювати з об'єктами різних типів, якщо вони пов'язані наслідуванням
-Так само, як вона працює з екземпляром Figure, вона може працювати з будь-яким наслідником цього класу
-
-*/
-
-
-/*
-Напишіть класс Circle, який реалізовує обчислення площі круга
-
-*/
-
-class Circle extends Figure {
-    constructor(r) {
-        super('circle');
-        this.r = r;
-    }
-
-    getArea() {
-        return Math.PI * this.r * this.r;
-    }
-}
+arrfn()
