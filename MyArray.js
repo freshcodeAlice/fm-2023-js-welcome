c30074f6452a0acadb41200bd53685dac8636570
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-29 10:07:05 +0300

    reduce method realization

--- MyArray.js
+++ MyArray.js
@@ -1,35 +1,36 @@
-function MyArray() {
-    this.length = 0;
+class MyArray {
+    constructor() {
+        this.length = 0;
 
-    for(let i = 0; i < arguments.length; i++) {
-        this[this.length] = arguments[i];
-        this.length++;
+        for(let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
     }
 
-}
-
-
-function MyProtoArray() {
-    this.push = function(value) {
+    push(value) {
         this[this.length] = value;
         this.length++;
     }
 
-    this.pop = function() {
+    pop() {
         const lastItem = this[this.length-1];
        delete this[this.length-1];
        this.length--;
        return lastItem;
     }
 
-    this.forEach = function(fn){ 
+    forEach(fn){ 
         for (let i = 0; i < this.length; i++) {
             fn(this[i]);
         }
     }
+
+    reduce(fn, initialValue) {
+        let accumulator = initialValue;
+        for(let i = 0; i < this.length; i++) {
+            accumulator = fn(accumulator, this[i], i);
+        }
+        return accumulator;
+    }
 }
-
-
-const protoArray = new MyProtoArray();
-MyArray.prototype = protoArray;
-
