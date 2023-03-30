318b9fade17ba29f37e77f6aa3a5ad229b1da42d
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-30 11:52:41 +0300

    flat problem

--- MyArray.js
+++ MyArray.js
@@ -33,4 +33,37 @@ class MyArray {
         }
         return accumulator;
     }
+
+    flat() {
+//        debugger;
+        const newArr = [];
+        for (let i = 0; i < this.length; i++) {
+            let item;
+            if (this[i] instanceof MyArray) { ////Array.isArray();
+                ///// робимо щось, якщо перед нами елмент-масив
+                // for (let j = 0 ; j < this[i].length; j++ ) {
+                //     newArr.push(this[i][j]);
+                // }
+                // item = this[i].flat(); <---- Проблема отут!
+            } else {
+                item = this[i];
+            }
+            newArr.push(item);
+        }
+        return newArr;
+    }
+
 }
+
+
+/*
+Реалізувати метод flat
+flat вирівнює масив, тобто створює новий, в якому елементи примітиви замість вкладеного масива
+
+
+const arr = [2, 3, 1, [2, 1, [3, 2], 1], 2, [2, 1, 2, 1, 2]]; ---> [2, 3, 1, 2, 1, 3, 2, 1, 2, 2, 1, 2, 1, 2]
+
+Задачка для роздумів - пофіксити наш flat, щоби він вирівнював багатовимірні масиви
+
+
+*/
