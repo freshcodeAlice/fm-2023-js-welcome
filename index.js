6260ca9d834fb52e8cab98b529470b90954212ca
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-30 09:58:00 +0300

    Double loop drowing tasks resolve

--- index.js
+++ index.js
@@ -1,8 +1,75 @@
 //// Подвійні цикли
 
-for(let i = 1; i <= 10; i++) {
-    debugger;
-    for (let k = 1; k <= 10; k++) {
-        console.log(`${i}x${k} = ${i*k}`);
+/// Намалювати в консолі квадрат 8*8 за допомогою символа "*"
+
+// let str = '';
+// for(let i = 0; i < 8; i++) {
+//     for(let j = 0; j < 8; j++) {
+//         str+='*';
+//     }
+//     str += '\n'
+// }
+
+
+// console.log(str);
+
+
+/// Намалювати трикутник 
+
+function drowTriangle(dim) {
+    let str = '';
+    for(let i = 0; i < dim; i++) {
+        for(let j = 0; j <= i; j++) {
+               str+='*';
+       }
+        str += '\n'
     }
+   
+   
+    console.log(str);
 }
+ 
+
+
+/// Задачка з *: Намалювати квадрат, всередині якого пусто
+
+function drowEmptySquare(dim) {
+    let str = '';
+    for(let i = 0; i < dim; i++) {
+        for(let j = 0; j < dim; j++) {
+            if(i === 0 || i === dim-1 || j === 0 || j === dim-1 ) {
+                str+='*';
+            } else {
+                str += ' ';
+            }
+    
+       }
+        str += '\n'
+    }
+    console.log(str);
+}
+
+
+//drowEmptySquare(10);
+
+/// Намалювати прямокутник зі зворотньою діагоналлю
+
+
+function drowEmptySquareWithDiagonal(dim) {
+    let str = '';
+    for(let i = 0; i < dim; i++) {
+        for(let j = 0; j < dim; j++) {
+            if(i === 0 || i === dim-1 || j === 0 || j === dim-1 || j === dim - i - 1) {
+                str+='*';
+            } else {
+                str += ' ';
+            }
+    
+       }
+        str += '\n'
+    }
+    console.log(str);
+}
+
+
+drowEmptySquareWithDiagonal(10);
