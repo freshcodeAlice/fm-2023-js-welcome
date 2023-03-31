c6045877d18113bb2d1d22ad5aabeef946fd6f37
freshcodeAlice <freshjs.ofte@gmail.com>
2023-03-31 10:55:55 +0300

    closure welcome

--- index.js
+++ index.js
@@ -1,50 +1,23 @@
+///Замикання
 
-/**
- * @param {String} name - Name of friend
- * @param {Number} amountCash - Amount of cash
- * @param {Friend} friend - friend of mine 
- */
+// Замыкание — это комбинация функции и лексического окружения, в котором эта функция была определена.
+
+let value = 10;
 
 
-class Friend {
 
-    constructor(name, amountCash, friend) {
-        this.name = name;
-        this.amountCash = amountCash;
-        this.friend = friend;
+function wrapper() {
+    let value = 0;
+    console.log('Wrapper function ---> ', value);
+
+    const logValue = function () {
+        console.log('LOG function --> ', value++);
     }
 
-    getAllCash() {
-
-        if (this.friend === null) {
-            return this.amountCash;
-        }
-        if(this.friend instanceof Friend) {
-            return this.amountCash + this.friend.getAllCash()
-        }
-
-        if(this.friend instanceof Array) {
-            return this.friend.reduce((accumulator, friend)=> accumulator + friend.getAllCash(), this.amountCash);
-        }
-    }
+    return logValue;
 }
 
-/*
-Написати метод getAllCash, який повертає загальну суму моїх грошей + грошей мого друга
+const anotherFunc = wrapper();
 
-*/
 
-const Fr = new Friend('Vik', 50, null);
-
-const I = new Friend('Alice', 20, null);
-
-const frfr = new Friend('Frfr', 50, null);
-
-/*
-В кожного екземпляра класу Friend може бути або null друзів, або один друг, або масив друзів, кожен з яких може мати null/Friend/[Friend]
-
-Переписати метод getAllCash на рекурсію, щоб він міг порахувати кошти всієї компанії
-
-*/
-
-const friendOfAll = new Friend('Kay', 50, [Fr, I, frfr]);
+const secondFunc = wrapper();
