c763e43a801badc27373dccfa220ac0e89effbf3
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-03 11:57:10 +0300

    One-Direction LinkedList

---
+++ LinkedList.js
@@ -0,0 +1,40 @@
+class LinkedItem {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    get value() {
+        return this._value
+    }
+    set value(v) {
+        this._value = v;
+    }
+}
+
+
+class LinkedList {
+    constructor(...args) {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    //    for (let i = 0; i < args.length; i++) {
+    //     this.push(args[i]);
+    //    }
+
+        args.forEach((el) => {this.push(el)});
+    }
+
+    push(value) {
+        const item = new LinkedItem(value);
+        if(this.length === 0) {  // якщо він перший
+        this.head = item;
+        this.tail = item;
+        } else { // якщо хтось вже тут стоїть
+            item.next = this.tail;
+            this.tail = item;
+
+        }
+        return ++this.length;
+    }
+}
--- index.js
+++ index.js
@@ -1,91 +1,14 @@
-//// Date
-
-
-/// Написати функцію, яка видає час у форматі:
-/// година*хвилина
-
-
-function getFormatDate() {
-    const date = new Date();
-    console.log(`${date.getHours()}:${date.getMinutes()}`);
+const data = {
+    u: 1,
+    a: 2,
+    user: 'a'
 }
 
 
-console.log(new Date().getHours());
+const arr = ['a', 'b', 'c', 'd', 'e', 'f'];
 
+arr.forEach((elem) => {
+  //  console.log(elem);
+})
 
-
-
-/*
-Написати функцію, яка виводить поточну дату у форматі:
-Mon, 3 April --- 10:09
-
-
-*/
-
-
-function getFormatTime() {
-    const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const date = new Date();
-    console.log(`${weekDays[date.getDay() - 1]}, ${date.getDate()} ${monthNames[date.getMonth()]} --- ${date.getHours()}:${date.getMinutes()}`);
-}
-
-
-/*
-Створити клас Student, при створенні екземпляра очікуємо на
-- ім'я 
-- прізвище
-- рік вступу
-
-Реалізувати методи:
-- getFullName(), який повертає повне ім'я
-- getCourse(), який повертає на якому курсі вчиться студент. Якщо > 5, то повернути "випустився".
-Рік обчислюємо відносно поточного року.
-
-
-
-*/
-
-
-
-class Student {
-    constructor(firstName, lastName, enterYear) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.enterYear = enterYear;
-    }
-
-
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`;
-    }
-
-    getCourse() {
-        // const year = new Date().getFullYear();
-        // const course = year - this.enterYear;
-        // if(course > 5) {
-        //     return 'graduated'
-        // } else {
-        //     return course
-        // }
-        const course = new Date().getFullYear() - this.enterYear;
-        return course > 5 ? "graduated" : course;
-    }
-}
-
-
-/*
-Отримати від користувача за допомогою prompt його дату народження
-'рік-місяць-день' і повернути кількість днів, що лишилась до дня народження
-
-
-*/
-
-function getMyBirthday() {
-    const userBirthday = prompt('Type your birthday');
-    const userBDate = new Date(userBirthday);
-    const bDateThisYear = new Date(new Date().getFullYear(), userBDate.getMonth(), userBDate.getDate());
-    const MsinDay = 1000 * 60 * 60 * 24;
-    return Math.floor((bDateThisYear.getTime() - Date.now()) / MsinDay);
-}
+arr.push('g');
