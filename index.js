c356a0d052b48bd5d777a6bff12a589f819bdf8d
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-03 09:36:02 +0300

    HW task resolve

--- index.js
+++ index.js
@@ -34,5 +34,78 @@ palindrom('mama') -> false
 (Голосні це "a", "e", "o", "u"," i").
 
 
-
 */
+
+
+function isSpam(stringToCheck) {
+    const spamWords = ['viagra', 'XXX', 'porn'];
+    const newStr = stringToCheck.toLowerCase();
+    for (let i = 0; i < spamWords.length; i++) {
+        if(newStr.includes(spamWords[i].toLowerCase())) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+/// buyViAGraNOW
+
+
+////2
+
+function truncate(str, length) {
+    if(str.length < length) {
+        return str;
+    }
+    // const newstr = str.slice(0, length);
+    // newstr += "...";
+    // return newstr;
+
+    return str.slice(0, length).concat('...');
+}
+
+
+////3
+
+function palindrom(str) {
+    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
+}
+
+
+
+
+/////4 
+
+/*
+Написати функцію, яка повертає кількість голосних символів в переданому рядку.
+(Голосні це "a", "e", "o", "u"," i").
+*/
+
+
+function countVowels(string) {
+    const vowels = ['a', 'e', 'o', 'u', 'i'];
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        for(let j = 0; j < vowels.length; j++) {
+            if(string[i] === vowels[j]) {
+                count++
+            }
+        }
+    }
+    return count;
+}
+
+
+function countVowels2(string) {
+    // const vowels = ['a', 'e', 'o', 'u', 'i'];
+    // const arr = string.split('');
+    // const resultArray = arr.filter((letter) => {
+    //     return vowels.includes(letter);
+    // });
+    // return resultArray.length;
+
+    return string.split('').filter(letter => ['a', 'e', 'o', 'u', 'i'].includes(letter)).length;
+}
