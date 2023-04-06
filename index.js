c71ccd7b7f7a8a822cfb4586d548dcd47166ef1e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-04-06 09:22:51 +0300

    Set

--- index.js
+++ index.js
@@ -1,63 +1,20 @@
-/// Map
+/// Set
 
-const map = new Map([['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']]);
+///  множина
+const set = new Set();
+
+set.add(3);
+set.add(4);
+set.add(5);
+
 
 /*
-map.set('key1', 'value1');
-map.set('key2', 'value2');
-map.set('key3', 'value3');
-*/
+Дан масив [2, 3,1, 2, 3, 2, 1]
+і масив [2, 1, 32, 6, 7, 23]
 
-
-/*
-Задача на анаграми: написати функцію, яка приймає два рядки і визначає, чи можемо ми відтворити другий рядок за допомогою літер першого рядка
-
-
-Декомпозиція задачі:
-1. Обидва слова треба привести до одного регістру
-2. Порахувати, скільки разів зустрічається та чи інша літера.
-
-Слова є анаграмами, якщо в них однакова кількість однакових літер
+Створити масив, що буде містити тільки унікальні значення з обох масивів
 
 */
-/**
- * 
- * @param {String} str1 - перше слово
- * @param {String} str2 - друге слово
- * @returns {Boolean} - true, якщо слова є анаграмами, false якщо не є
- */
-
-function compare(str1, str2) {
-    const map1 = createVocabulary(str1.toLowerCase());
-    const map2 = createVocabulary(str2.toLowerCase());
-  
-    if(map1.size !== map2.size) {
-        return false
-    }
-
-    for (const key of map1.keys()) {
-        if (map1.get(key) !== map2.get(key)){
-            return false
-        }
-    }
-    return true
-}
-
-
-function createVocabulary(str) {
-    const map = new Map();
-    for (let i=0; i < str.length; i++) {
-        if(map.has(str[i])) { /// літера мені вже раніше зустрічалась
-           let letterCount = map.get(str[i]);
-           map.set(str[i], letterCount+1);
-        } else { /// літера мені не зустрічалась ще жодного разу
-            map.set(str[i], 1);
-        }
-       
-    }
-    return map;
-}
-
-
-// mama -> amma
-// mama -> mamamam
+const arr1 = [2, 3,1, 2, 3, 2, 1];
+const arr2 = [2, 1, 32, 6, 7, 23];
+const array = [...new Set([...arr1, ...arr2])]; 
